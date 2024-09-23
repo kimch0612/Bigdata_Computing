6c3167afd8961d8fbacada084e5a2a1297506e98
Changhwan Kim <kimch061279@gmail.com>
2024-09-23 14:28:35 +0900

    a

--- week3/test.py
+++ week3/test.py
@@ -1,5 +1,14 @@
-import requests, json
+# import requests, json
 
-r = requests.get("https://v6.exchangerate-api.com/v6/ea6ef78e98f0b9230fccc229/latest/KRW")
-dt_dict = r.json()
-print(dt_dict['conversion_rates'])
+# r = requests.get("https://v6.exchangerate-api.com/v6/ea6ef78e98f0b9230fccc229/latest/KRW")
+# dt_dict = r.json()
+# print(dt_dict['conversion_rates'])
+
+import requests, json, time
+
+for _ in range (0, 5):
+    r = requests.get("http://api.open-notify.org/iss-now.json")
+    r_dict = r.json()
+    print(r_dict["iss_position"])
+    time.sleep(5)
+    _ += 1
